2861acb926221784403ee4d7acf9876e460a2cc6
Spruce Feinstein <sprucefeinstein@gmail.com>
2023-12-13 21:53:54 -0600

    much smoother sprite movement

--- src/pixi/Sprite.tsx
+++ src/pixi/Sprite.tsx
@@ -1,38 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Sprite, useTick } from '@pixi/react';
 
 const MySprite = () => {
-    const speed = 1;
+    const speed = 3;
     const [position, setPosition] = useState({ x: 0, y: 0 });
-    const [movement, setMovement] = useState({ x: 0, y: 0 });
+    const [keys, setKeys] = useState({ up: false, down: false, left: false, right: false});
 
-    const handleKeyDown = (event: any) => {
-        switch (event.key) {
-            case 'ArrowUp':
-                setMovement({ x: 0, y: -speed });
-                break;
-            case 'ArrowDown':
-                setMovement({ x: 0, y: speed });
-                break;
-            case 'ArrowLeft':
-                setMovement({ x: -speed, y: 0 });
-                break;
-            case 'ArrowRight':
-                setMovement({ x: speed, y: 0 });
-                break;
-            default:
-                break;
-        }
-    };
-
-    const handleKeyUp = () => {
-        setMovement({ x: 0, y: 0 });
-    };
+    const handleKeyDown = useCallback((event: any) => {
+        setKeys(prevKeys => {
+            switch (event.key) {
+                case 'ArrowUp':
+                    return { ...prevKeys, up: true };
+                case 'ArrowDown':
+                    return { ...prevKeys, down: true };
+                case 'ArrowLeft':
+                    return { ...prevKeys, left: true };
+                case 'ArrowRight':
+                    return { ...prevKeys, right: true };
+                default:
+                    return prevKeys;
+            }
+        });
+    }, []);
+    
+    const handleKeyUp = useCallback((event: any) => {
+        setKeys(prevKeys => {
+            switch (event.key) {
+                case 'ArrowUp':
+                    return { ...prevKeys, up: false };
+                case 'ArrowDown':
+                    return { ...prevKeys, down: false };
+                case 'ArrowLeft':
+                    return { ...prevKeys, left: false };
+                case 'ArrowRight':
+                    return { ...prevKeys, right: false };
+                default:
+                    return prevKeys;
+            }
+        });
+    }, []);
 
     useTick((delta) => {
+
+        const newY = keys.up && !keys.down ? -speed : (!keys.up && keys.down ? speed : 0)
+        const newX = keys.left && !keys.right ? -speed : (!keys.left && keys.right ? speed : 0)
+
+
         setPosition(prev => ({
-            x: prev.x + movement.x * delta,
-            y: prev.y + movement.y * delta
+            x: prev.x + newX * delta,
+            y: prev.y + newY * delta
         }));
     });
 
@@ -43,7 +59,7 @@ const MySprite = () => {
             window.removeEventListener('keydown', handleKeyDown);
             window.removeEventListener('keyup', handleKeyUp);
         };
-    }, []);
+    }, [handleKeyDown, handleKeyUp]);
 
     return <Sprite  image="https://pixijs.io/pixi-react/img/bunny.png" x={position.x} y={position.y} />;
 };
