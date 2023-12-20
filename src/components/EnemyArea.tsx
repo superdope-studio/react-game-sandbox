d066b4543e720cfb7107f022653518e46440fd01
Spruce Feinstein <sprucefeinstein@gmail.com>
2023-12-20 17:26:00 -0600

    Adding turn state, drawing hands of 3 cards for both player and enemy, beginning of enemy AI

--- src/components/EnemyArea.tsx
+++ src/components/EnemyArea.tsx
@@ -1,16 +1,48 @@
-import { Box } from "@mui/material";
+import { Box, Card, CardContent } from "@mui/material";
 import { useGameState } from "../contexts/GameStateContext";
+import { GameCard } from "../data/cards";
+
+const CardBack = () => {
+  return (
+    <Box sx={{ margin: "8px" }}>
+      <Card>
+        <CardContent>No Lookie</CardContent>
+      </Card>
+    </Box>
+  );
+};
 
 export const EnemyArea = () => {
   const { gameState } = useGameState();
 
   return (
-    <Box
-      sx={{ display: "flex", justifyContent: "space-around", width: "100vw" }}
-    >
-      <Box>Energy: {gameState.enemyEnergy}</Box>
-      <Box>Enemy Area</Box>
-      <Box>Health: {gameState.enemyHealth}</Box>
+    <Box>
+      <Box
+        sx={{ display: "flex", justifyContent: "space-around", width: "100vw" }}
+      >
+        <Box>Energy: {gameState.enemyEnergy}</Box>
+        <Box>
+          {!gameState.playerTurn && (
+            <Box sx={{ fontWeight: 600 }}>Current Turn</Box>
+          )}
+          Enemy Area
+        </Box>
+        <Box>Health: {gameState.enemyHealth}</Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          maxHeight: "33vh",
+          overflow: "scroll",
+        }}
+      >
+        {gameState.enemyHand.map((card: GameCard, idx) => (
+          <Box key={idx}>
+            <CardBack />
+          </Box>
+        ))}
+      </Box>
     </Box>
   );
 };
--- src/data/cards.ts
+++ src/data/cards.ts
@@ -38,3 +38,36 @@ export const playerDeck: GameCard[] = [
     type: "Attack",
   },
 ];
+
+export const enemyDeck: GameCard[] = [
+  {
+    title: "Flame Jet",
+    damage: 4,
+    energyCost: 3,
+    type: "Attack",
+  },
+  {
+    title: "Gear Strike",
+    damage: 3,
+    energyCost: 2,
+    type: "Attack",
+  },
+  {
+    title: "Steam Blast",
+    damage: 5,
+    energyCost: 4,
+    type: "Attack",
+  },
+  {
+    title: "Bolt Throw",
+    damage: 2,
+    energyCost: 1,
+    type: "Attack",
+  },
+  {
+    title: "Cog Barrage",
+    damage: 6,
+    energyCost: 5,
+    type: "Attack",
+  },
+];
