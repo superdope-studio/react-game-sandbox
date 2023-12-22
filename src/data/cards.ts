93609ec8b4911a7941fcb77eed7408a71daa7f5a
Justin Rhoads <rhoadsjustin@gmail.com>
2023-12-22 11:22:55 -0600

    couple decks, select decks, save decks

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -6,6 +6,87 @@ export type GameCard = {
   type: "Attack" | "Defense" | "Utility";
 };
 
+export type GameDeck = {
+  id: number;
+  title: string;
+  cards: GameCard[];
+};
+
+export const exampleDecks: GameDeck[] = [
+  {
+    id: 1,
+    title: 'Attack Deck',
+    cards: [
+      {
+        title: "Flame Jet",
+        damage: 4,
+        energyCost: 3,
+        type: "Attack",
+      },
+      {
+        title: "Gear Strike",
+        damage: 3,
+        energyCost: 2,
+        type: "Attack",
+      },
+      {
+        title: "Steam Blast",
+        damage: 5,
+        energyCost: 4,
+        type: "Attack",
+      },
+      {
+        title: "Bolt Throw",
+        damage: 2,
+        energyCost: 1,
+        type: "Attack",
+      },
+      {
+        title: "Cog Barrage",
+        damage: 6,
+        energyCost: 5,
+        type: "Attack",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: 'Defense Deck',
+    cards: [
+      {
+        title: "Flame Jet",
+        damage: 4,
+        energyCost: 3,
+        type: "Attack",
+      },
+      {
+        title: "Gear Strike",
+        damage: 3,
+        energyCost: 2,
+        type: "Attack",
+      },
+      {
+        title: "Steam Blast",
+        damage: 5,
+        energyCost: 4,
+        type: "Attack",
+      },
+      {
+        title: "Bolt Throw",
+        damage: 2,
+        energyCost: 1,
+        type: "Attack",
+      },
+      {
+        title: "Cog Barrage",
+        damage: 6,
+        energyCost: 5,
+        type: "Attack",
+      },
+    ],
+  },
+]
+
 export const playerDeck: GameCard[] = [
   {
     title: "Flame Jet",
